19e4dc0f28796691e31797c120e2c2a7478975fb
youngman <61752860+sksn12@users.noreply.github.com>
2023-06-15 02:24:27 +0900

    [Gold V] Title: A와 B 2, Time: 136 ms, Memory: 14512 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static String S;
+    static boolean val=false;
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        S=br.readLine();
+        String T= br.readLine();
+
+        recursive(T);
+
+        if(val){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+    }
+
+    private static void recursive(String t) {
+        if(S.length()==t.length()){
+            if(S.equals(t))val=true;
+            return;
+        }
+        if(t.charAt(0)=='B'){
+            String reverseStr="";
+            for (int i = t.length()-1; 0<i  ; i--) {
+                reverseStr+=String.valueOf(t.charAt(i));
+            }
+            recursive(reverseStr);
+        }
+
+        if(t.charAt(t.length()-1)=='A'){
+            String str="";
+            for (int i = 0; i<t.length()-1; i++) {
+                str+=String.valueOf(t.charAt(i));
+            }
+            recursive(str);
+        }
+
+    }
+}

8a6c08355f0185f41ce6221266f1486304524076
youngman <61752860+sksn12@users.noreply.github.com>
2023-07-13 15:16:56 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT id,name,host_id
+from places
+where host_id in (select host_id from places
+                  group by host_id
+                  having count(id)>=2)
+order by id

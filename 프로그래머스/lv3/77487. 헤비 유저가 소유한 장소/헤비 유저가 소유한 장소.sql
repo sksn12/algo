39f0c6947afe6b81affffdf704ba7e390832b733
youngman <61752860+sksn12@users.noreply.github.com>
2023-07-25 13:00:34 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,7 +1,8 @@
 -- 코드를 입력하세요
-SELECT id,name,host_id
+SELECT *
 from places
 where host_id in (select host_id from places
-                  group by host_id
-                  having count(id)>=2)
+            group by host_id
+            having count(*)>=2
+         )
 order by id

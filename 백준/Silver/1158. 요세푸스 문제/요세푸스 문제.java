f724dfa1192dadfea7870c7f362af171a9fbf243
youngman <61752860+sksn12@users.noreply.github.com>
2025-03-24 12:38:19 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 548 ms, Memory: 15364 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,31 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
 
         int N=Integer.parseInt(st.nextToken());
-        int M=Integer.parseInt(st.nextToken());
-        StringBuilder sb=new StringBuilder();
-        Queue<Integer> q=new LinkedList<>();
+        int K=Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++)q.add(i+1);
+        int[] map=new int[N+1];
+        boolean[] v=new boolean[N+1];
+        List<Integer> list=new ArrayList<>();
 
-        sb.append("<");
-        while (1<q.size()){
-            for (int i = 0; i < M-1; i++) {
-                q.add(q.poll());
+        for(int i=1;i<=N;i++)map[i]=i;
+
+        int cnt=0;
+        int L=1;
+        while(true){
+
+            // 모든 값이 들어갔으면 종료
+            if(list.size()==N){
+                break;
             }
-            sb.append(q.poll()+", ");
+
+            // 아직 현재 위치의 값이 나가지 않았다면 해당값은 카운트 or 정답에 넣거나
+            if(!v[L]){
+                cnt+=1;
+                if(cnt==K){
+                    cnt=0;
+                    v[L]=true;
+                    list.add(map[L]);
+                }
+            }
+
+            if(L==N)L=1;
+            else L+=1;
+
         }
 
-        sb.append(q.poll()+">");
+        StringBuilder sb=new StringBuilder();
+        sb.append("<");
+
+        for(int i=0;i<list.size();i++){
+            if(i==list.size()-1){
+                sb.append(list.get(i)+">");
+            }else{
+                sb.append(list.get(i)+", ");
+            }
+        }
 
         System.out.println(sb);
     }

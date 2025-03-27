f223b562f1e52f64aee3d0990801a521f7622340
youngman <61752860+sksn12@users.noreply.github.com>
2025-03-27 11:42:08 +0900

    [Gold V] Title: 치킨 배달, Time: 208 ms, Memory: 17136 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,76 +1,65 @@
+import java.util.*;
+import java.io.*;
 
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
-// 치킨집의 조합을 구해서 각 조합마다 모든 시뮬레이션을 돌려보고 치킨거리가 가장 적게 나오는 값을 출력
 public class Main {
-	static ArrayList<int[]> homeList=new ArrayList<>();
-	static ArrayList<int[]> chickenList=new ArrayList<>();
-	static int N;
-	static int M;
-	static int result=Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(bf.readLine());
-		
-		N=Integer.parseInt(st.nextToken());
-		M=Integer.parseInt(st.nextToken());
-		
-		for (int i = 0; i < N; i++) {
-			st=new StringTokenizer(bf.readLine());
-			for (int j = 0; j < N; j++) {
-				int tmp=Integer.parseInt(st.nextToken());
-				if(tmp==1) {
-					homeList.add(new int[] {i,j});
-				}else if(tmp==2) {
-					chickenList.add(new int[] {i,j});
-				}
-			}
-		}
+    static int N,M;
+    static List<int[]> chicken=new ArrayList<>();
+    static List<int[]> home=new ArrayList<>();
+    static int[] path;
+    static int answer=Integer.MAX_VALUE;
 
-		recursive(new boolean[chickenList.size()],0,0);
-		System.out.println(result);
-		
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
 
-	private static void recursive(boolean[] sel, int idx, int k) {
-		if(idx==sel.length) {
-			if(k==M) {
-				ArrayList<int []> combineList=new ArrayList<>();
-				for (int i = 0; i < sel.length; i++) {
-					if(sel[i]) {
-						combineList.add(new int[] {chickenList.get(i)[0],chickenList.get(i)[1]});
-					}
-				}
-				result=Math.min(distance(combineList), result);
-			}
-			return;
-		}
-	
-		sel[idx]=true;
-		recursive(sel, idx+1, k+1);
-		sel[idx]=false;
-		recursive(sel, idx+1, k);
-	}
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
 
-	private static int distance(ArrayList<int[]> combineList) {
-		int sum=0;
-		for (int i = 0; i < homeList.size(); i++) {
-			int SD=Integer.MAX_VALUE;
-			int[] tmp=homeList.get(i);
-			for (int j = 0; j < combineList.size(); j++) {
-				int[] tmp2=combineList.get(j);
-				int D=Math.abs(tmp[0]-tmp2[0])+Math.abs(tmp[1]-tmp2[1]);
-				SD=Math.min(SD, D);
-			}
-			sum+=SD;
-		}
-		return sum;
-	}
+        for(int i=0;i<N;i++){
+            st=new StringTokenizer(br.readLine());
+            for(int j=0;j<N;j++){
+                int k=Integer.parseInt(st.nextToken());
+
+                if(k==1) home.add(new int[] {i,j});
+                else if (k==2) chicken.add(new int[] {i,j});
+            }
+        }
+
+        path=new int[M];
+        recursive(0,0);
+
+        System.out.println(answer);
+    }
+
+    public static void recursive(int start,int level){
+        if(level==M){
+            int sum=0;
+
+            for(int i=0;i<home.size();i++){
+                // 현재 집에서 가장 가까운 치킨 거리 찾기
+                int nowD=Integer.MAX_VALUE;
+                int hy=home.get(i)[0];
+                int hx=home.get(i)[1];
+
+                for(int j=0;j<path.length;j++){
+                    int cy=chicken.get(path[j])[0];
+                    int cx=chicken.get(path[j])[1];
+
+                    int innerD=Math.abs(hy-cy)+Math.abs(hx-cx);
+                    nowD=Math.min(nowD,innerD);
+                }
+
+                sum+=nowD;
+            }
+
+            answer=Math.min(answer,sum);
+            return;
+        }
+
+        for(int i=start;i<chicken.size();i++){
+            // 치킨집의 인덱스 번호 넣기
+            path[level]=i;
+            recursive(i+1,level+1);
+        }
+    }
 }

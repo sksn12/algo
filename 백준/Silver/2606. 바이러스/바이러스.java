566abedd1d7568870965f5215dccb6c9d2a6fe27
youngman <61752860+sksn12@users.noreply.github.com>
2025-04-10 09:32:20 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main{
     static int N;
-    static boolean[][] map;
+    static List<List<Integer>> map;
     static boolean[] v;
     static int answer=0;
 
@@ -16,8 +16,12 @@ public class Main{
         st=new StringTokenizer(br.readLine());
         int Z=Integer.parseInt(st.nextToken());
 
-        map=new boolean[N+1][N+1];
+        map=new ArrayList<>();
         v=new boolean[N+1];
+
+        for(int i=0;i<=N;i++){
+            map.add(new ArrayList<>());
+        }
 
         for(int z=0;z<Z;z++){
             st=new StringTokenizer(br.readLine());
@@ -25,9 +29,9 @@ public class Main{
             int x1=Integer.parseInt(st.nextToken());
             int x2=Integer.parseInt(st.nextToken());
 
-            map[x1][x2]=true;
-            map[x2][x1]=true;
-
+            map.get(x1).add(x2);
+            map.get(x2).add(x1);
+            
         }
 
         dfs(1);
@@ -38,10 +42,10 @@ public class Main{
     public static void dfs(int node){
         v[node]=true;
 
-        for(int i=1;i<=N;i++){
-            if(!v[i] && map[node][i]){
-                answer+=1;
-                dfs(i);
+        for(int j=0;j<map.get(node).size();j++){
+            if(!v[map.get(node).get(j)]){
+                answer += 1;
+                dfs(map.get(node).get(j));
             }
         }
     }

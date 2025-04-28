479f22e6eb72fa26d31b31f66403f1b77b4996ff
youngman <61752860+sksn12@users.noreply.github.com>
2025-04-28 18:56:13 +0900

    [level 2] Title: 올바른 괄호, Time: 18.38 ms, Memory: 55.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<Character> stack=new Stack<>();
+        
+        Character[] C=new Character[s.length()];
+        
+        for(int i=0;i<s.length();i++){
+            C[i]=s.charAt(i);
+        }
+        
+        for(Character c:C){
+            if(c=='(')stack.push(c);
+            else{
+                if(stack.size()==0)return false;
+                
+                Character tmp=stack.pop();
+                if(tmp!='('){
+                    return false;
+                }
+            }
+        }
+        
+        if(stack.size()!=0)return false;
+
+        return answer;
+    }
+}

7cb323c2bab6a3014cf741ee721b07c9bb6a50f1
youngman <61752860+sksn12@users.noreply.github.com>
2025-05-02 14:49:51 +0900

    [level 2] Title: 더 맵게, Time: 1451.79 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -6,21 +6,22 @@ class Solution {
         
         PriorityQueue<Integer> pq=new PriorityQueue<>();
         
-        for(int i=0;i<scoville.length;i++){
-            pq.offer(scoville[i]);
+        for(int s:scoville){
+            pq.offer(s);
         }
         
         while(true){
             if(pq.size()<2){
                 int low=pq.poll();
                 
-                if(low>=K)return answer;
-                else return -1;
+                return (low>=K)? answer:-1;
             }
             
             int Low=pq.poll();
             
-            if(Low>=K)return answer;
+            if(Low>=K){
+                return answer;
+            }
             else{
                 int second=pq.poll();
                 answer+=1;

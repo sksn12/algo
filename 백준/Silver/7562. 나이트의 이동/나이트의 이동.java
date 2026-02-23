f6ad2437febdd04e5cb00c8c37385cdf3cb416e6
youngman <sksn12@naver.com>
2026-02-23 17:11:59 +0900

    [Silver I] Title: 나이트의 이동, Time: 240 ms, Memory: 41216 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,72 +1,69 @@
-import java.util.*;
+
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N,ey,ex;
-    static int[] dy={-2,-1,1,2,2,1,-1,-2};
-    static int[] dx={1,2,2,1,-1,-2,-2,-1};
-    static int[][] map;
-
-    static class Node{
+    static int N;
+    static int[] dy={-1,-2,-2,-1,1,2,2,1};
+    static int[] dx={-2,-1,1,2,2,1,-1,-2};
+    static Queue<chass> q;
+    static boolean[][] map;
+    static class chass{
         int y,x,cnt;
 
-        Node(int y,int x,int cnt){
+        chass(int y,int x,int cnt){
             this.y=y;
             this.x=x;
             this.cnt=cnt;
         }
     }
-    static Queue<Node> q;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int T=Integer.parseInt(st.nextToken());
 
-        for(int tc=0;tc<T;tc++){
-            st=new StringTokenizer(br.readLine());
-            N=Integer.parseInt(st.nextToken());
-
-            q=new LinkedList<>();
+        for (int tc = 0; tc < T; tc++) {
+            N=Integer.parseInt(br.readLine());
+            q=new ArrayDeque<>();
+            map=new boolean[N][N];
 
             st=new StringTokenizer(br.readLine());
             int sy=Integer.parseInt(st.nextToken());
             int sx=Integer.parseInt(st.nextToken());
-
-            q.offer(new Node(sy,sx,0));
+            q.offer(new chass(sy, sx,0));
 
             st=new StringTokenizer(br.readLine());
-            ey=Integer.parseInt(st.nextToken());
-            ex=Integer.parseInt(st.nextToken());
-
-            map=new int[N][N];
+            int ey=Integer.parseInt(st.nextToken());
+            int ex=Integer.parseInt(st.nextToken());
 
             if(sy==ey && sx==ex){
                 System.out.println(0);
                 continue;
             }
-            BFS();
+
+            map[sy][sx]=true;
+            BFS(ey,ex);
         }
     }
 
-    public static void BFS(){
-        while(!q.isEmpty()){
-            Node n=q.poll();
+    public static void BFS(int ey,int ex){
+        while (!q.isEmpty()){
+            chass c=q.poll();
 
-            for(int d=0;d<8;d++){
-                int ny=n.y+dy[d];
-                int nx=n.x+dx[d];
+            for (int d = 0; d < 8; d++) {
+                int ny=c.y+dy[d];
+                int nx=c.x+dx[d];
 
-                if(0<=ny && ny<N && 0<=nx && nx<N && map[ny][nx]==0){
-                    map[ny][nx]=1;
-
-                    if(ny==ey && nx==ex){
-                        System.out.println(n.cnt+1);
+                if(0<=ny && ny<N && 0<=nx && nx<N && !map[ny][nx]){
+                    if(ey==ny && ex==nx){
+                        System.out.println(c.cnt+1);
                         return;
                     }
 
-                    q.offer(new Node(ny,nx,n.cnt+1));
+                    map[ny][nx]=true; // 방문 했던 기준을 통해 시간복잡도를 줄임, 어차피 BFS는 최단거리임으로 뒤에 오는 순서는 해당 경로를 거치고 최단경로가 될수없음(목적지에 도착하는 경료여도 제일빨리간놈만)
+                    q.offer(new chass(ny,nx,c.cnt+1));
                 }
             }
         }

33a101ab593590625826defdd108153977455689
youngman <sksn12@naver.com>
2026-02-25 15:26:17 +0900

    [Gold III] Title: 여왕벌, Time: 3532 ms, Memory: 262872 KB, Score: 83 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.java
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class Main {
     static int M,N;
     static int[][] map;
-    static int[][] grow;
     static int sy,sx;
     static int[] dy={0,-1,-1};
     static int[] dx={-1,-1,0};
@@ -31,8 +30,6 @@ public class Main {
             int one=Integer.parseInt(st.nextToken());
             int two=Integer.parseInt(st.nextToken());
 
-            grow=new int[M][M];
-
             // 시작 위치
             sy=M-1; sx=0;
 
@@ -43,7 +40,6 @@ public class Main {
 
             for (int j = one; j>0 ; j--) {
                 map[sy][sx]+=1;
-                grow[sy][sx]=1;
 
                 if(sy<=0) sx+=1;
                 else sy-=1;
@@ -51,38 +47,31 @@ public class Main {
 
             for (int j = two; j>0 ; j--) {
                 map[sy][sx]+=2;
-                grow[sy][sx]=2;
 
                 if(sy<=0) sx+=1;
                 else sy-=1;
             }
-
-//            System.out.println(i+1 +"일차 대각쪽 먼저 증가 완료");
-//            print();
-//            System.out.println("-----------------------");
-
-            for (int j = 1; j < M; j++) {
-                for (int k = 1; k < M; k++) {
-                    int max=0;
-
-                    for (int d = 0; d < 3; d++) {
-                        int ny=j+dy[d];
-                        int nx=k+dx[d];
-
-                        if(0<=ny && ny<M && 0<=nx && nx<M){
-                            max=Math.max(grow[ny][nx],max);
-                        }
-                    }
-
-                    grow[j][k]=max;
-                    map[j][k]+=max;
-                }
-            }
-
-//            System.out.println(i+1 +"일차 나머지 증가 완료");
-            print();
-//            System.out.println("-----------------------");
         }
+
+        for (int j = 1; j < M; j++) {
+            for (int k = 1; k < M; k++) {
+                int max=0;
+
+                for (int d = 0; d < 3; d++) {
+                    int ny=j+dy[d];
+                    int nx=k+dx[d];
+
+                    if(0<=ny && ny<M && 0<=nx && nx<M){
+                        max=Math.max(map[ny][nx],max);
+                    }
+                }
+
+                map[j][k]=max;
+            }
+        }
+
+        print();
+
     }
 
     public static void print(){
